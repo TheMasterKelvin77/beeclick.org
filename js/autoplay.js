59da12f35dff2f62e038fa7d17d16af477b96514
Kevin Robb <kevin.robb@ou.edu>
2020-05-11 18:19:32 -0500

    Fix autoclick and autoplay strategy

--- js/autoplay.js
+++ js/autoplay.js
@@ -7,23 +7,32 @@ function autoplayPress() {
 
 function autoplay() {
     // click the bee
-    $(function() {
-        var e = $.Event('keypress');
-        e.which = 32; // spacebar
-        $(document).trigger(e);
-    });
+    // $(function() {
+    //     var e = $.Event('keypress');
+    //     e.which = 32; // spacebar
+    //     $(document).trigger(e);
+    // });
+    var e = $.Event('keypress');
+    e.which = 32; // spacebar
+    $(document).trigger(e);
+    // easy way if this doesn't work -> Game.clicky();
 
     // save money to afford "Beetopia" before buying "Dont Lose Your Way"
-    if (Game.upgrades[Game.era].id == 7 && Game.upgrades["Super Factory"].count > 10) {
+    if (Game.upgrades[Game.era].id == 7 && Game.upgrades["Super Factory"].count >= 10) {
         if (Game.honey >= 1000010) {
-            Game.buyUpgrade("Dont Lose Your Way");
+            if (Game.getUpgradePurchasable("Dont Lose Your Way")){
+                Game.buyUpgrade("Dont Lose Your Way");
+            }
+        }
+    } else if (Game.upgrades[Game.era].id == 8) {
+        if (Game.getUpgradePurchasable("Beetopia")){
             Game.buyUpgrade("Beetopia");
         }
     } else {
         // buy each available upgrade
         for (var x in Game.upgrades) {
             //upgrade = Game.upgrades[x];
-            if (Game.upgrades[x].id != 8 && Game.getUpgradePurchasable(x)){
+            if (Game.upgrades[x].id < 8 && Game.getUpgradePurchasable(x)){
                 Game.buyUpgrade(x);
             }
         }
